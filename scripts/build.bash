48b77ee8c85a2df2405fa2ad2d9cab70b506e63e
Stoney Jackson <dr.stoney@gmail.com>
2025-06-08 20:41:40 +0000

    build html, pdf, and pptx

--- scripts/build.bash
+++ scripts/build.bash
@@ -1,13 +1,45 @@
 #!/usr/bin/env bash
 
-main() {
-    set -euo pipefail
-    echo "Building the project..."
+set -euo pipefail
+ME=$(basename "$0")
 
-    bin/plantuml -tsvg -filedir "/data" -o /data/build/slides/ch2 /data/slides/ch2
-    cp slides/ch2/*.md build/slides/ch2
-    
-    echo "Build completed successfully."
+
+info() {
+    f=$1
+    shift
+    >&2 echo "[  INFO  ] $ME:$f(): $*"
 }
+
+
+main() {
+    info $FUNCNAME "Building the project..."
+    mkdir -p build
+    cp -R slides build
+    build-plantuml
+    build-marp
+    info $FUNCNAME "Build completed successfully."
+}
+
+
+build-plantuml() {
+    info $FUNCNAME "Building SVG diagrams..."
+    for d in $(find slides -type d); do
+        info $FUNCNAME "Processing: $d/"
+        bin/plantuml -failfast -tsvg "/data/build/$d"
+    done
+}
+
+
+build-marp() {
+    info $FUNCNAME "Building HTML slides..."
+    bin/marp -I "/home/marp/app/build"
+
+    info $FUNCNAME "Building PDF slides..."
+    bin/marp -I "/home/marp/app/build" --pdf
+
+    info $FUNCNAME "Building PPTX slides..."
+    bin/marp -I "/home/marp/app/build" --pptx
+}
+
 
 main "$@"
